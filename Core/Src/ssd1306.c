bfe90880d2bd505e950f6324a150a9b4a6724032
t3ry4 <t3ry4mail@gmail.com>
2025-06-04 00:42:43 +0300

    Limit I2C multi-write count

--- Core/Src/ssd1306.c
+++ Core/Src/ssd1306.c
@@ -568,14 +568,17 @@ extern I2C_HandleTypeDef hi2c1;
 			p--;
 	}
 
-	void ssd1306_I2C_WriteMulti(uint8_t address, uint8_t reg, uint8_t* data, uint16_t count) {
-		uint8_t dt[256];
-		dt[0] = reg;
-		uint8_t i;
-		for(i = 0; i < count; i++)
-		dt[i+1] = data[i];
-		HAL_I2C_Master_Transmit(SSD1306_I2C, address, dt, count+1, 10);
-	}
+void ssd1306_I2C_WriteMulti(uint8_t address, uint8_t reg, uint8_t* data, uint16_t count) {
+        uint8_t dt[256];
+        dt[0] = reg;
+        if (count > 255) {
+                count = 255;
+        }
+        uint8_t i;
+        for(i = 0; i < count; i++)
+        dt[i+1] = data[i];
+        HAL_I2C_Master_Transmit(SSD1306_I2C, address, dt, count+1, 10);
+}
 
 
 	void ssd1306_I2C_Write(uint8_t address, uint8_t reg, uint8_t data) {
